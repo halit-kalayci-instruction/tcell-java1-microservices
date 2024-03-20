f69f5f2cbf102ef162a5bcc5879b854b49de3788
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-03-20 21:31:31 +0300

    discovery server

--- customerservice/src/main/java/com/turkcell/customerservice/CustomerserviceApplication.java
+++ customerservice/src/main/java/com/turkcell/customerservice/CustomerserviceApplication.java
@@ -2,10 +2,13 @@ package com.turkcell.customerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+// @EnableDiscoveryClient => Eski versiyonlarda gerekli idi..
+// @EnableEurekaClient
 public class CustomerserviceApplication {
 
 	public static void main(String[] args) {
--- customerservice/src/main/java/com/turkcell/customerservice/clients/OrderServiceClient.java
+++ customerservice/src/main/java/com/turkcell/customerservice/clients/OrderServiceClient.java
@@ -4,6 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+// TODO: Use discovery server (araştırma ve uygulama)
+// TODO: Load balancing (araştırma)
 @FeignClient(name="orderservice", url="http://localhost:8081")
 public interface OrderServiceClient
 {
