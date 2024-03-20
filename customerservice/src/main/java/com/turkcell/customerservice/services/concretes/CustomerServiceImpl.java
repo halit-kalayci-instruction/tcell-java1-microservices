f0e52cce3dba7300b7ab4830a1bd6c6448e20977
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-03-20 21:00:28 +0300

    add spring cloud

--- customerservice/src/main/java/com/turkcell/customerservice/services/concretes/CustomerServiceImpl.java
+++ customerservice/src/main/java/com/turkcell/customerservice/services/concretes/CustomerServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
-
+// 8:55
 @Service
 @RequiredArgsConstructor
 public class CustomerServiceImpl implements CustomerService {
