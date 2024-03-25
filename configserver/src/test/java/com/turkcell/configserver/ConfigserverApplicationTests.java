e99d62eaf7098ae76d01c5132d4a93719270f450
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-03-25 20:29:24 +0300

    config server

---
+++ configserver/src/test/java/com/turkcell/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turkcell.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
