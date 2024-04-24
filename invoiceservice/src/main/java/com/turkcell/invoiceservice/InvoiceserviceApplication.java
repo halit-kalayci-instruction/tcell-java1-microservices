bcad101d8231c3d6bc2273af08f1bc5d63c9d595
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-04-24 21:40:16 +0300

    .

--- invoiceservice/src/main/java/com/turkcell/invoiceservice/InvoiceserviceApplication.java
+++ invoiceservice/src/main/java/com/turkcell/invoiceservice/InvoiceserviceApplication.java
@@ -1,13 +1,20 @@
 package com.turkcell.invoiceservice;
 
+import com.turkcell.events.OrderCreatedEvent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 public class InvoiceserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InvoiceserviceApplication.class, args);
+	}
+
+	@KafkaListener(topics = "orderTopic", groupId = "invoiceGroup")
+	public void handleEvent(OrderCreatedEvent event) {
+		System.out.println("Bir mesaj yakalandı..");
 	}
 
 }
--- orderservice/src/main/java/com/turkcell/orderservice/controllers/OrdersController.java
+++ orderservice/src/main/java/com/turkcell/orderservice/controllers/OrdersController.java
@@ -1,15 +1,19 @@
 package com.turkcell.orderservice.controllers;
 
+import com.turkcell.events.OrderCreatedEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/v1/orders")
+@RequiredArgsConstructor
 public class OrdersController
 {
+    private final KafkaTemplate<String, OrderCreatedEvent> kafkaTemplate;
     @GetMapping
     public int getCustomerIdByOrderId(@RequestParam String orderId)
     {
@@ -20,5 +24,11 @@ public class OrdersController
         }
         catch(Exception e){}*/
         return 10;
+    }
+    @PostMapping
+    public String addOrder()
+    {
+        kafkaTemplate.send("orderTopic","NewOrderEvent", new OrderCreatedEvent(1, LocalDateTime.now()));
+        return "Sipariş eklendi..";
     }
 }
